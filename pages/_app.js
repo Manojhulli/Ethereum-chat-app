adb4695d7aec62808ec29f5b50cd754dc1cc9fdc
Beast-Sanjay <sanjuniky2002@gmail.com>
2022-11-11 19:08:55 +0530

    chat context created

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 
+import { ChatAppProvider } from '../context/ChatAppContext';
+import {NavBar} from '../components/index';
+
+
 const MyApp = ({ Component, pageProps }) => (
   <div>
-    <Component {...pageProps} />
+    <ChatAppProvider>
+      <NavBar/>
+      <Component {...pageProps} />
+    </ChatAppProvider>
+    
   </div>
 );
 
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,34 @@
-import React from "react"
 
-const ethchatapp =() =>{
-  return(
-    <div>ChatApp</div>
-  )
-}
+// import React,{useEffect, useState, useContext} from "react";
 
-export default ethchatapp
+// import { ChatAppContect } from "../Context/ChatAppContext";
+
+// import { Filter, Friend } from "../Components/index";
+
+// const ethchatapp =() =>{
+//   const {title} = useContext(ChatAppContect);
+//   return(
+//     <div>{title}</div>
+//   )
+// }
+
+// export default ethchatapp
+
+import React, { useEffect, useState, useContext } from "react";
+
+//INTERNAL IMPORT
+import { ChatAppContect } from "../Context/ChatAppContext";
+import { Filter, Friend } from "../Components/index";
+
+const ChatApp = () => {
+   const {title} = useContext(ChatAppContect);
+  return (
+    <div>
+      {title}
+      {/* <Filter />
+      <Friend /> */}
+    </div>
+  );
+};
+
+export default ChatApp;
