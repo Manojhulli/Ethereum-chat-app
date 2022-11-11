3428ee5f5725b3205dcb1d0726052f23735f74af
Manoj Hulli <0.manojhulli@gmail.com>
2022-11-11 16:29:55 +0530

    created the api for fetch contract and time

--- utils/apiFeature.js
+++ utils/apiFeature.js
@@ -34,5 +34,37 @@ export const connectWallet = async () => {
     }
   };
 
-  
-  
+  const fetchContract = (signerOrProvider) => 
+    new ethers.Contract(ChatAppABI,ChatAppAddress,signerOrProvider);
+
+export const connectingWithContract = async () =>{
+  try {
+    const web3model = new web3model();
+    const connection = await web3model.connect();
+    const provider = new ethers.provides.Web3Provider(connection);
+    const signer =provider.getSigner();
+    const contract = fetchContract(signer);
+
+    return contract;
+  }
+    catch (error){
+      console.log(error);
+  }
+};
+
+export const converTime = (time)=> {
+  const newTime = new Date(time.toNumber());
+
+  const realTime = newTime.getHours() + "/" + 
+  newTime.getMinutes() + 
+  "/"+
+  newTime.getSeconds()+
+  " Date:"+
+  newTime.getDate()+
+  "/"+
+  (newTime.getMonth()) + 1; + 
+  "/" + 
+  newTime.getFullYear();
+
+  return realTime;
+};
