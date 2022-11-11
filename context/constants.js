bd4f2c277b39ff9371fa74b0dbee738000c4c485
Beast-Sanjay <sanjuniky2002@gmail.com>
2022-11-11 16:09:36 +0530

    added the block address

---
+++ context/constants.js
@@ -0,0 +1,6 @@
+//0x5fbdb2315678afecb367f032d93f642f64180aa3
+import EchatappJSON from './echatapp.json'
+
+export const chatappAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+
+export const ChatAppABI = EchatappJSON.abi;
